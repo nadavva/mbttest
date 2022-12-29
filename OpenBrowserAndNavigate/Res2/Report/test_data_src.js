d77d8633cc44ffe61b012e593328be957ac18846
Nadav Vanunu <nadavva@gmail.com>
2022-12-29 13:56:47 +0200

    initial tests, launchBrowser & register
    initial tests, launchBrowser & register

---
+++ OpenBrowserAndNavigate/Res2/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\jenkins\\Desktop\\MBTtests\\OpenBrowserAndNavigate\\Res2',namePairs:[]}]};
---
+++ OpenBrowserAndNavigate/Res5/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\jenkins\\Desktop\\MBTtests\\OpenBrowserAndNavigate\\Res5',namePairs:[]}]};
